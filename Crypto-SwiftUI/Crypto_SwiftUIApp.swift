a2bddae81cd651e05540a48bda9704be7e09d899
Barış Akçay <embarisakcay@gmail.com>
2024-05-14 16:22:21 +0300

    Initial Commit

---
+++ Crypto-SwiftUI/Crypto_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_SwiftUIApp.swift
+//  Crypto-SwiftUI
+//
+//  Created by Baris Akcay on 14.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
